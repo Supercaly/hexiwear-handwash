85f55c9122c077f3602d594a620913d3d5f3186b
Lorenzo Calisti <djonline96@gmail.com>
2022-03-04 10:40:57 +0100

    Generate new assets and fix wrist menu problem

--- application/src/menu_page.cpp
+++ application/src/menu_page.cpp
@@ -15,29 +15,34 @@ Page page_stats_menu = {
 };
 Page page_wrist_menu = {
     .left = &page_settings_menu,
+    .right = &page_wrist_selection,
+    .image = wrist_menu_bmp,
+};
+Page page_wrist_selection = {
+    .left = &page_wrist_menu,
     .action = [](DisplayWrapper *display)
     {
         display->wrist(!display->wrist());
         if (display->wrist())
         {
-            display->image(wrist_left_content_bmp, 20, 20);
+            display->image(wrist_ct_left_bmp, 20, 20);
         }
         else
         {
-            display->image(wrist_right_content_bmp, 20, 20);
+            display->image(wrist_ct_right_bmp, 20, 20);
         }
     },
-    .image = wrist_menu_bmp,
-};
-Page page_wrist_left = {
-    .left = &page_wrist_menu,
-    .right = &page_wrist_right,
-    .image = wrist_left_bmp,
-};
-Page page_wrist_right = {
-    .left = &page_wrist_menu,
-    .right = &page_wrist_left,
-    .image = wrist_right_bmp,
+    .onDraw = [](DisplayWrapper *display)
+    {
+        if (display->wrist())
+        {
+            display->image(wrist_ct_left_bmp, 20, 20);
+        } else 
+        {
+            display->image(wrist_ct_right_bmp, 20, 20);
+        }
+    },
+    .image = wrist_selection_bg_bmp,
 };
 Page page_stats = {
     .left = &page_stats_menu,
@@ -51,7 +56,9 @@ Page page_stats = {
         // sprintf(san_str, "%d", d->san_count());
         // d->label(none_str, 50, 30);
         // d->label(wash_str, 50, 50);
-        d->label((const char *)d->none_count() + 33, 50, 70);
+        d->label("ciao", 58, 8);
+        d->label("ciao", 58, 40);
+        d->label((const char *)"ciao", 58, 64);
     },
-    .image = stats_bmp,
+    .image = stats_bg_bmp,
 };
--- application/src/menu_page.h
+++ application/src/menu_page.h
@@ -18,12 +18,11 @@ struct Page
     const uint8_t *image;
 };
 
+// Predefined pages used by the application
 extern Page page_settings_menu;
 extern Page page_stats_menu;
 extern Page page_wrist_menu;
-// TODO: Replace left and right pages with one and draw the content depending on local variables
-extern Page page_wrist_left;
-extern Page page_wrist_right;
+extern Page page_wrist_selection;
 extern Page page_stats;
 
 #endif // MENU_PAGE_H_
