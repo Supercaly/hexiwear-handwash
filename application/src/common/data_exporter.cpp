631499471954e983395680ca06da37c82848cdcc
Lorenzo Calisti <djonline96@gmail.com>
2022-07-01 11:21:01 +0200

    Make data exporter export data as json
    This commit changes `data_exporter` to export the data in the uSD card as JSON. Before we where exporting the data as binary, like it's saved in the internal memory, but now we convert each chunck to a JSON onject and pretty-print it to a file for better readability and usage.

--- application/src/common/data_exporter.cpp
+++ application/src/common/data_exporter.cpp
@@ -2,11 +2,14 @@
 #include "common/global_thread_vars.h"
 #include "common/log.h"
 
-#include "FATFileSystem.h"
-#include "FileSystem.h"
-#include "SDBlockDevice.h"
+DataExporter::DataExporter()
+{
+    _internal_bd = BlockDevice::get_default_instance();
+    _internal_fs = FileSystem::get_default_instance();
 
-DataExporter::DataExporter() {}
+    _sd_bd = new SDBlockDevice(PTE3, PTE1, PTE2, PTE4);
+    _sd_fs = new FATFileSystem("sd");
+}
 
 DataExporter::~DataExporter() {}
 
@@ -18,76 +21,135 @@ bool DataExporter::export_data()
         return false;
     }
 
-    FileSystem *fs = FileSystem::get_default_instance();
-    if (fs->mount(BlockDevice::get_default_instance()) < 0)
+    if (!mount_fs(_internal_fs, _internal_bd))
     {
-        log_error("Cannot mount filesystem for reading logs!\n");
         return false;
     }
     else
     {
-        FILE *f = fopen("/fs/raw_data.txt", "rb");
-        if (f == NULL)
+        void *data_buffer;
+        long data_size;
+
+        if (!open_file("/fs/raw_data.txt", "rb", &_internal_f))
         {
-            log_error("Cannot open log file %s(%d)\n", strerror(errno), errno);
-            fs->unmount();
-            return false;
+            goto exit_with_error;
         }
         else
         {
-            void *copy_buff;
+            // Read content from internal memory
+            fseek(_internal_f, 0L, SEEK_END);
+            data_size = ftell(_internal_f);
+            fseek(_internal_f, 0L, SEEK_SET);
+            log_info("total internal file size: %ld bytes\n", data_size);
 
-            fseek(f, 0L, SEEK_END);
-            long sz = ftell(f);
-            fseek(f, 0L, SEEK_SET);
-            log_info("log file size: %ld bytes\n", sz);
-
-            copy_buff = malloc(sz);
-            if (copy_buff == NULL)
+            data_buffer = malloc(data_size);
+            if (data_buffer == NULL)
             {
-                log_error("Cannot allocate memory for coping\n");
-                return false;
+                log_error("DataExporter: cannot allocate memory for coping data!\n");
+                goto exit_with_error;
             }
 
-            fread(copy_buff, 1, sz, f);
+            fread(data_buffer, 1, data_size, _internal_f);
             printf("log file: '");
-            for (int i = 0; i < sz; i++)
+            for (int i = 0; i < data_size; i++)
             {
-                printf("%02X ", ((char *)copy_buff)[i]);
+                printf("%02X ", ((char *)data_buffer)[i]);
             }
             printf("'\n");
 
-            fclose(f);
-            fs->unmount();
+            // Close internal file and unmount internal filesystem
+            fclose(_internal_f);
+            _internal_fs->unmount();
 
-            SDBlockDevice sd(PTE3, PTE1, PTE2, PTE4);
-            FATFileSystem sdfs("sd");
-            if (sdfs.mount(&sd) < 0)
+            // Mount SD filesystem
+            if (!mount_fs(_sd_fs, _sd_bd))
             {
-                log_error("Cannot mount sd\n");
-                goto exit_error;
+                goto exit_with_error;
             }
             else
             {
-                FILE *sdf = fopen("/sd/raw_data.txt", "wb+");
-                if (sdf == NULL)
+                if (!open_file("/sd/raw_data.txt", "wb+", &_sd_f))
                 {
-                    log_error("Cannot open file on sd\n");
-                    goto exit_error;
+                    goto exit_with_error;
                 }
                 else
                 {
-                    fwrite(copy_buff, sizeof(char), sz, sdf);
+                    // Format and write the data to the SD card
+                    fprintf(_sd_f, "{\n");
+                    for (int i = 0; i < data_size; i += 107)
+                    {
+                        format_data(_sd_f, (uint8_t *)data_buffer + i);
+                        if (i != data_size)
+                        {
+                            fprintf(_sd_f, ",\n");
+                        }
+                        else
+                        {
+                            fprintf(_sd_f, "\n");
+                        }
+                    }
+                    fprintf(_sd_f, "}");
+
                     log_info("File exported\n");
-                    fclose(sdf);
-                    sdfs.unmount();
-                    return true;
+
+                    fclose(_sd_f);
+                    _sd_fs->unmount();
                 }
             }
-
-        exit_error:
-            free(copy_buff);
-            return false;
         }
+        free(data_buffer);
+        return true;
+
+    exit_with_error:
+        // Unmount all filesystems and free allocated memory
+        _internal_fs->unmount();
+        _sd_fs->unmount();
+        if (data_buffer != NULL)
+        {
+            free(data_buffer);
+        }
+        return false;
     }
+}
+
+bool DataExporter::mount_fs(FileSystem *fs, BlockDevice *bd)
+{
+    if (fs->mount(bd) < 0)
+    {
+        log_error("DataExporter: cannot mount filesystem!");
+        return false;
+    }
+    return true;
+}
+
+bool DataExporter::open_file(const char *path, const char *mode, FILE **f)
+{
+    *f = fopen(path, mode);
+    if (*f == NULL)
+    {
+        log_error("DataExporter: cannot open file '%s' (%s)\n", path, strerror(errno));
+        return false;
+    }
+    return true;
+}
+
+void DataExporter::format_data(FILE *file, void *data)
+{
+    fprintf(file, "  {\n");
+    fprintf(file, "    \"label\": %d,\n", *((uint8_t *)data + 6));
+    fprintf(file, "    \"features\": [");
+
+    float *feats = (float *)((uint8_t *)data + 7);
+    for (int i = 0; i < 25; i++)
+    {
+        if (i != 0)
+        {
+            fprintf(file, ",");
+        }
+        fprintf(file, "%f", feats[i]);
+    }
+    fprintf(file, "]\n");
+    // TODO: Format the raw data when they'll be recorded
+
+    fprintf(file, "  }");
 }
--- application/src/common/data_exporter.h
+++ application/src/common/data_exporter.h
@@ -1,6 +1,12 @@
 #ifndef DATA_EXPORTER_H_
 #define DATA_EXPORTER_H_
 
+#include "mbed.h"
+#include "BlockDevice.h"
+#include "FATFileSystem.h"
+#include "FileSystem.h"
+#include "SDBlockDevice.h"
+
 class DataExporter
 {
 public:
@@ -8,6 +14,21 @@ public:
     ~DataExporter();
 
     bool export_data();
+
+private:
+    FileSystem
+        *_internal_fs,
+        *_sd_fs;
+    BlockDevice
+        *_internal_bd,
+        *_sd_bd;
+    FILE
+        *_internal_f,
+        *_sd_f;
+
+    bool mount_fs(FileSystem *fs, BlockDevice *bd);
+    bool open_file(const char *path, const char *mode, FILE **f);
+    void format_data(FILE *file, void *data);
 };
 
 #endif // DATA_EXPORTER_H_
