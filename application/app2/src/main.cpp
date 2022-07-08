144c1530468994aaa967c4924ceca4cab8e4484b
Lorenzo Calisti <djonline96@gmail.com>
2022-07-08 10:01:52 +0200

    Move main.cpp inside src folder

---
+++ application/app2/src/main.cpp
@@ -0,0 +1,28 @@
+#include "FATFileSystem.h"
+#include "mbed.h"
+
+// Override default console for enabling printf
+FileHandle *mbed::mbed_override_console(int fd)
+{
+    static BufferedSerial serial_out(USBTX, USBRX, 9600);
+    return &serial_out;
+}
+
+// Define default filesystem
+FileSystem *FileSystem::get_default_instance()
+{
+    static FATFileSystem fs("sd");
+    return &fs;
+}
+
+int main()
+{
+    DigitalOut status_led(LED_RED);
+    while (true)
+    {
+        status_led = !status_led;
+        ThisThread::sleep_for(500ms);
+    }
+
+    return 0;
+}
