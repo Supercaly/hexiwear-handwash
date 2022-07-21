6d2ce20f9af49465bc8668b5586f1ea85acd636b
Lorenzo Calisti <djonline96@gmail.com>
2022-07-21 17:54:34 +0200

    Change split_database to generate multiple files

--- training/split_database.py
+++ training/split_database.py
@@ -14,43 +14,47 @@ from data_loader import load_raw_data
 @click.option("-s", "--percent", type=click.FloatRange(0.0, 1.0), default=0.80, show_default=True, help="Percentage of data put into the training set compared to that in the test set.")
 def main(src, window_size, freq, dst, percent):
     """Loads the database .csv file and split it indo training and
-    testing sets creating two different .csv files.
+    testing sets.
     """
     dst = os.path.join(dst, str(window_size))
 
     # load raw data
     raw_data = iter(load_raw_data(src))
 
-    # open destination files
-    os.makedirs(dst, exist_ok=True)
-    train_file = open(os.path.join(dst, "train.csv"), "w+")
-    test_file = open(os.path.join(dst, "test.csv"), "w+")
+    # ensure destination directories
+    train_dir = os.path.join(dst, "train")
+    test_dir = os.path.join(dst, "test")
+    os.makedirs(train_dir, exist_ok=True)
+    os.makedirs(test_dir, exist_ok=True)
 
     print(
-        f"Splitting database '{src}' in training and testing inside folder '{dst}' with {percent*100}% split. This may take some time...")
+        f"Splitting database '{src}' in '{train_dir}' and '{test_dir}' with {percent*100}% split.")
+    print("This may take some time...")
 
     # split raw data by window_size
     chunk = list(islice(raw_data, window_size * freq))
 
-    # prepare csv writers
-    train_csv = csv.DictWriter(train_file, chunk[0].keys())
-    test_csv = csv.DictWriter(test_file, chunk[0].keys())
-    train_csv.writeheader()
-    test_csv.writeheader()
-
     # write chunks in training file or test file
     # a chunk has 80% probability of going in the
     # training set adn 20% probability in test set.
+    train_cnt = 0
+    test_cnt = 0
     while chunk:
         if np.random.uniform() < percent:
-            train_csv.writerows(chunk)
+            # write to training set
+            file_path = os.path.join(train_dir, f"{train_cnt}.csv")
+            train_cnt += 1
         else:
-            test_csv.writerows(chunk)
-        chunk = list(islice(raw_data, window_size * freq))
+            # write to test set
+            file_path = os.path.join(test_dir, f"{test_cnt}.csv")
+            test_cnt += 1
 
-    # close csv files
-    train_file.close()
-    test_file.close()
+        with open(file_path, "wb+") as f:
+            w = csv.DictWriter(f, chunk[0].keys())
+            w.writeheader()
+            w.writerows(chunk)
+
+        chunk = list(islice(raw_data, window_size * freq))
 
 
 if __name__ == "__main__":
