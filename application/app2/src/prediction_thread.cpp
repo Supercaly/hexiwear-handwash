4ed7d0a53f817cd0974bd7f9f31792d9cf47207d
Lorenzo Calisti <djonline96@gmail.com>
2022-07-24 12:36:50 +0200

    Add ML_MODE to cmake script in order to select wich ml model to use

--- application/app2/src/prediction_thread.cpp
+++ application/app2/src/prediction_thread.cpp
@@ -5,7 +5,9 @@
 #include "label.h"
 #include "log.h"
 #include "raw_sensor_data.h"
+#ifdef HAS_SVM
 #include "svm.h"
+#endif // HAS_SVM
 
 void prediction_thread_loop()
 {
@@ -13,7 +15,9 @@ void prediction_thread_loop()
     Label actual_label,
         pred_label;
     static RawSensorData raw_chunk;
+#ifdef HAS_SVM
     SVM svm;
+#endif // HAS_SVM
 
     // Init importer
     if (!importer.init())
@@ -22,18 +26,22 @@ void prediction_thread_loop()
         return;
     }
 
+#ifdef HAS_SVM
     // Init SVM
     if (!svm.init("/sd/model_10.model"))
     {
         log_error("error loading SVM model from file\n");
         return;
     }
+#endif // HAS_SVM
 
     while (importer.next_chunk(&raw_chunk, &actual_label))
     {
         log_info("chunk of data read\n");
 
+#ifdef HAS_SVM
         svm.predict(&raw_chunk, &pred_label);
+#endif // HAS_SVM
 
         log_info("predicted label: %s actual label: %s\n",
                  label_to_cstr(pred_label),
