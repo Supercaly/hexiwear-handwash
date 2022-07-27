af948c658aadd1e9f45682bcd9b2a5c190bd4f69
Lorenzo Calisti <djonline96@gmail.com>
2022-07-27 11:41:55 +0200

    Add ability to run ml modes to prediction_thread

--- application/app2/src/prediction_thread.cpp
+++ application/app2/src/prediction_thread.cpp
@@ -5,8 +5,13 @@
 #include "label.h"
 #include "log.h"
 #include "raw_sensor_data.h"
+// Include the correct ml library
 #ifdef HAS_SVM
 #include "svm.h"
+#elif defined(HAS_ML)
+#include "predictor.h"
+#else
+#error cannot find a machine learning library!
 #endif // HAS_SVM
 
 void prediction_thread_loop()
@@ -15,8 +20,13 @@ void prediction_thread_loop()
     Label actual_label,
         pred_label;
     static RawSensorData raw_chunk;
+    Timer inference_timer;
+
 #ifdef HAS_SVM
     SVM svm;
+#elif defined(HAS_ML)
+    Predictor predictor;
+    PredictorError status;
 #endif // HAS_SVM
 
     // Init importer
@@ -25,6 +35,7 @@ void prediction_thread_loop()
         log_error("error initializing DataImporter\n");
         return;
     }
+    log_info("DataImporter init!\n");
 
 #ifdef HAS_SVM
     // Init SVM
@@ -33,18 +44,45 @@ void prediction_thread_loop()
         log_error("error loading SVM model from file\n");
         return;
     }
+    log_info("SVM init!\n");
+#elif defined(HAS_ML)
+    // Init Predictor
+    status = predictor.init();
+    if (status != PredictorError::OK)
+    {
+        log_error("error initializing Predictor: %s\n",
+                  predictor_error_to_cstr(status));
+        return;
+    }
+    log_info("Predictor init!\n");
 #endif // HAS_SVM
 
-    while (importer.next_chunk(&raw_chunk, &actual_label))
+    int counter = 0;
+    while (importer.next_chunk(&raw_chunk, &actual_label) && counter < 100)
     {
-        log_info("chunk of data read\n");
+        log_info("computing inference on new chunk of data...\n");
 
 #ifdef HAS_SVM
         svm.predict(&raw_chunk, &pred_label);
+#elif defined(HAS_ML)
+        inference_timer.reset();
+        inference_timer.start();
+        // TODO: Remove wrist option since it's unused
+        status = predictor.predict_label(&raw_chunk, Wrist::LEFT, &pred_label);
+        if (status != PredictorError::OK)
+        {
+            log_error("fatal error predicting label: %s\n",
+                      predictor_error_to_cstr(status));
+            return;
+        }
+        inference_timer.stop();
 #endif // HAS_SVM
 
-        log_info("predicted label: %s actual label: %s\n",
+        log_info("predicted label: %s actual label: %s "
+                 "in: %lldms\n",
                  label_to_cstr(pred_label),
-                 label_to_cstr(actual_label));
+                 label_to_cstr(actual_label),
+                 chrono::duration_cast<chrono::milliseconds>(inference_timer.elapsed_time()).count());
+        counter++;
     }
 }
